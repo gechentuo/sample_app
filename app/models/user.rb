9c3e0d8baf92eaf0ae9bd972376be20e5f00a93f
Gechentuo <200893255@qq.com>
2013-09-07 12:59:31 +0800

    Add user microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
-  
+ 
+ has_many :microposts
+
  before_save {self.email = email.downcase}
  before_create :create_remember_token
 
@@ -8,6 +10,10 @@ class User < ActiveRecord::Base
  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false}
  has_secure_password
  validates :password, length:{ minimum: 6}
+
+ def feed
+  Micropost.where("user_id=?", id)
+ end
 
  def User.new_remember_token
   SecureRandom.urlsafe_base64
